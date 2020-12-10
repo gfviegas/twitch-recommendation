b1d382809a5a79f390e2a598af2bf963d42479e6
vitor-l <vitorluisgs@gmail.com>
2020-12-10 19:37:07 -0300

    Estiliza dashboard

--- trs/lib/trs/streams.ex
+++ trs/lib/trs/streams.ex
@@ -4,8 +4,12 @@ defmodule TRS.Streams do
   # TODO: Bater na api da twitch pra retornar estes dados
   def list_streams do
     [
-      %Stream{id: 1, user_id: 23090, user_name: "JOaozinho Gameplay", game_id: 27471, title: "Joguinho da quebrada", viewer_count: 10, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-{width}x{height}.jpg"},
-      %Stream{id: 2, user_id: 23090, user_name: "JOaozinho Gameplay", game_id: 18122, title: "TO alone venham me ver", viewer_count: 5, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-{width}x{height}.jpg"}
+      %Stream{id: 1, user_id: 23090, user_name: "JOaozinho Gameplay", game_name: "Minecraft", game_id: 27471, title: "Joguinho da quebrada", viewer_count: 10, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-1280x720.jpg"},
+      %Stream{id: 2, user_id: 23090, user_name: "JOaozinho Gameplay", game_name: "Minecraft", game_id: 18122, title: "TO alone venham me ver", viewer_count: 5, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-1280x720.jpg"},
+      %Stream{id: 3, user_id: 23090, user_name: "JOaozinho Gameplay", game_name: "Minecraft", game_id: 18122, title: "TO alone venham me ver", viewer_count: 5, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-1280x720.jpg"},
+      %Stream{id: 4, user_id: 23090, user_name: "JOaozinho Gameplay", game_name: "Minecraft", game_id: 18122, title: "TO alone venham me ver", viewer_count: 5, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-1280x720.jpg"},
+      %Stream{id: 5, user_id: 23090, user_name: "JOaozinho Gameplay", game_name: "Minecraft", game_id: 18122, title: "TO alone venham me ver", viewer_count: 5, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-1280x720.jpg"},
+      %Stream{id: 6, user_id: 23090, user_name: "JOaozinho Gameplay", game_name: "Minecraft", game_id: 18122, title: "TO alone venham me ver", viewer_count: 5, thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-1280x720.jpg"}
     ]
   end
 
--- trs/lib/trs_web/endpoint.ex
+++ trs/lib/trs_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule TRSWeb.Endpoint do
     at: "/",
     from: :trs,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt eye.svg)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
